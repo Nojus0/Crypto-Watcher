c47edd5baf6064118b12ab9477eea109b03c9b19
Nojus0 <35661992+Nojus0@users.noreply.github.com>
2021-01-19 08:21:52 +0000

    Update?

--- src/server.js
+++ src/server.js
@@ -4,11 +4,15 @@ const connection = new WebSocket("wss://stream.coinmarketcap.com/price/latest");
 
 connection.onmessage = ({ data }) => {
     const COIN = new Crypto(JSON.parse(data));
+    if(COIN.TYPE === COIN_TYPE.BTC)
+    {
+        console.log(`BTC PRICE: ${COIN.price}, LAST HOUR: ${COIN.LastHour} LAST 24 HOURS: ${COIN.Lat24Hours}, LAST 7 DAYS: ${COIN.Last7Days}, VOLUME: ${COIN.volume}`);
+    }
 }
 
 connection.onopen = () => {
-    console.log("Connected to server.");
-    CLIENT_SUBSCRIBE.send(connection, [COIN_TYPE.BTC, COIN_TYPE.ETH]);
+    console.log("Connected to coinmarketcap.com server.");
+    CLIENT_SUBSCRIBE.send(connection, [COIN_TYPE.BTC]);
 }
 
 connection.onerror = err => {
